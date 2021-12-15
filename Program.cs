edc9db2b415046eb2bdc3546549735a2c261accc
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 14:19:05 +0100

    amélioré la lisibilité des consignes pour l'utilisateur.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 string ajout = "";
 while (ajout != "non")
 {
-    Console.WriteLine("Ecrivez un nombre à additionner");
+    Console.WriteLine("Ecrivez un nombre à additionner et appuyez sur entrée.");
     result += int.Parse(Console.ReadLine());
-    Console.WriteLine("Voulez-vous rajouter un nombre?");
+    Console.WriteLine("Appuyez sur entrée pour continuer à rajouter un nombre.\nEcrivez non et appuyez sur entrée pour arrêter.");
     ajout = Console.ReadLine();
 }
 Console.WriteLine($"Le résultat final est {result}.");
