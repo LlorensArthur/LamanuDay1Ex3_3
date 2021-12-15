5fff4d3d6dd492d9609a04edf97624c788f90316
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 16:45:05 +0100

    Limité la saisie utilisateur aux nombres pour l'addition.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿int result = 0;
-string ajout = "";
-while (ajout != "non")
+int toAdd;
+string endString = "";
+while (endString != "non")
 {
     Console.WriteLine("Ecrivez un nombre à additionner et appuyez sur entrée.");
-    result += int.Parse(Console.ReadLine());
+    while (!int.TryParse(Console.ReadLine(), out toAdd))
+    {
+        Console.WriteLine("Veuillez entrer uniquement un nombre.");
+    }
+    result += toAdd;
     Console.WriteLine("Appuyez sur entrée pour continuer à rajouter un nombre.\nEcrivez non et appuyez sur entrée pour arrêter.");
-    ajout = Console.ReadLine();
+    endString = Console.ReadLine();
 }
 Console.WriteLine($"Le résultat final est {result}.");
