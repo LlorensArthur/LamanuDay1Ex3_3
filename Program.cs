a1710e6ba2bb3f2b67114cd877ae30634159322b
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:20:16 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int result = 0;
+string ajout = "";
+while (ajout != "non")
+{
+    Console.WriteLine("Ecrivez un nombre à additionner");
+    result += int.Parse(Console.ReadLine());
+    Console.WriteLine("Voulez-vous rajouter un nombre?");
+    ajout = Console.ReadLine();
+}
+Console.WriteLine($"Le résultat final est {result}.");
